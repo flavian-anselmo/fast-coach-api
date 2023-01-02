99167266cd7b3232085d335a21530c3077166e73
flavian-anselmo <anselmoflavian4@gmail.com>
2023-01-02 14:32:24 +0300

    payment logic restructure

--- app/routers/payments.py
+++ app/routers/payments.py
@@ -21,21 +21,19 @@ router = APIRouter(
 
 
 
-async def change_paid_status(db, amount:float, ticket_id:int, curr_user_id:int):
+async def change_paid_status(db, ticket_id:int, curr_user_id:int):
     '''
-    change the paid status in booking table to paid 
+    change the paid status in booking table to paid = True
+    This methos is initiated only when the payment process is succefull 
 
     '''
     try:
         paid_status = db.query(models.BookTicket).filter(models.BookTicket.ticket_id == ticket_id).first()
         if paid_status.is_paid == False:
-            # make the change 
             paid_status.is_paid = True
             db.commit()
             db.refresh(paid_status)
-            if paid_status.is_paid:
-                await notify_passenger_via_sms(db, curr_user_id, amount)
-            return paid_status
+            return paid_status.is_paid
     except Exception as error:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(error))
 
@@ -45,27 +43,23 @@ async def change_paid_status(db, amount:float, ticket_id:int, curr_user_id:int):
 
 
 
-async def notify_passenger_via_sms(db, curr_user_id:int, amount:float):
+async def notify_passenger_via_sms(db, curr_user_id:int):
     '''
     after the user has paid notify them via sm 
+    Notify them if the payment was succesfull or not 
+
+    if no -> then retyr the process after 5 minutes as a background task 
 
     '''
 
     user = db.query(models.User).filter(models.User.user_id == curr_user_id).first()
     if user:
-            payment = await payment_process_via_africans_talking(
-                product_name = 'Fast.Coach.API',
-                phone_number = user.phone_number,
-                currency_code = 'KES',
-                amount = amount
-            )
-
-            print(payment)
-
-            if payment:
-                sms_response = await SMS().send_sms(recipient=[user.phone_number], msg= f'Hello {user.last_name}, thanks for booking with fastcoach!')
-                print(sms_response)
-                return sms_response
+        sms_response = await SMS().send_sms(
+            recipient=[user.phone_number],
+            msg= f'Hello {user.last_name}, thanks for booking with fastcoach!'
+        )
+        print(sms_response)
+        return sms_response
 
   
         
@@ -76,19 +70,35 @@ async def notify_passenger_via_sms(db, curr_user_id:int, amount:float):
 
 
 
-async def payment_process_via_africans_talking(product_name:str, phone_number:str, currency_code:str, amount:float):
+async def payment_process_via_africans_talking(db, curr_user_id:int, ticket_id:int, amount:float):
     '''
     actual payment process initation 
     (stkpush via africas_talking api)
 
     '''
     try:
-        await PaymentService().checkout(
-            productName = product_name,
-            phoneNumber = phone_number,
-            currencyCode = currency_code,
-            amount = amount
-        )
+        user = db.query(models.User).filter(models.User.user_id == curr_user_id).first()
+        if user:
+            print('payment:part')
+            payment = await PaymentService().checkout(
+                productName = 'Fast.Coach.API',
+                phoneNumber = user.phone_number,
+                currencyCode = 'KES',
+                amount = amount
+            )
+            if payment:
+                print('notification:part')
+                notification = await notify_passenger_via_sms(
+                    db,
+                    curr_user_id,
+                )
+                if notification:
+                    print('paid_status:part')
+                    await change_paid_status(
+                        db,
+                        ticket_id,
+                        curr_user_id
+                    )
     except Exception as error:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=str(error))
 
@@ -102,17 +112,20 @@ async def payment_process_via_africans_talking(product_name:str, phone_number:st
 async def pay_for_ticket(payament:schemas.PaymentCreate, db:session = Depends(get_db), curr_user = Depends(oauth2.get_current_user_logged_in)):
     '''
     initiate payment with africas talking stkpush  
+    After payments are successfull a message is sent to the passenger notifying them of the booking 
 
     '''
     try:
         pay = models.Payments( **payament.dict(), user_id = curr_user.user_id)
         db.add(pay)
         db.commit()
-        await change_paid_status(
-            db = db,
-            amount =  payament.amount,
-            ticket_id = payament.ticket_id,
-            curr_user_id = curr_user.user_id
+
+        ''' initiate payment '''
+        await payment_process_via_africans_talking(
+            db,
+            curr_user.user_id,
+            payament.ticket_id,
+            payament.amount
         )
         db.refresh(pay)
         return pay
