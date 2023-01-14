edff234c63fdb8c7c271f8f5fd94a23537d8dc3d
flavian-anselmo <anselmoflavian4@gmail.com>
2023-01-14 20:06:46 +0300

    deployment setup (railway)

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from app.config import settings
-SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}/{settings.database_name}'
-
+# SQLALCHEMY_DATABASE_URL =  f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_host}/{settings.database_name}'
+SQLALCHEMY_DATABASE_URL = f'postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}'
 engine = create_engine(
 
     SQLALCHEMY_DATABASE_URL
